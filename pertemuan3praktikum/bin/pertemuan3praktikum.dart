c207e7c3d124d840211c0938826e2355d52adaa5
MiniMaliZ <gakisomlebu@gmail.com>
2025-09-16 13:56:54 +0700

    Membuat Project Baru untuk tugas praktikum

---
+++ pertemuan3praktikum/bin/pertemuan3praktikum.dart
@@ -0,0 +1,5 @@
+import 'package:pertemuan3praktikum/pertemuan3praktikum.dart' as pertemuan3praktikum;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pertemuan3praktikum.calculate()}!');
+}
